3cc1f867f05310af4caac2e933f738eca2549642
alapczvk <115263643+alapczvk@users.noreply.github.com>
2022-12-15 11:01:47 +0100

    prawie moj kalkulator

--- lab6/src/newCalculator.java
+++ lab6/src/newCalculator.java
@@ -4,11 +4,22 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
 
-public class newCalculator {
+public class newCalculator implements ActionListener {
+    boolean zeroError = false;
+    boolean equation = true;
+
+    enum Input {
+        NUMBER,
+        EQUAL,
+        ACTION,
+    }
+    Input lastInput = Input.ACTION;
     public static void createAndShowGUI() {
         JFrame jf = new JFrame("Calculator");
+        newCalculator calculator = new newCalculator();//stworzenie obiektu
         jf.setVisible(true);
         jf.setPreferredSize(new Dimension(390, 300));
         jf.setResizable(false); //
@@ -17,7 +28,7 @@ public class newCalculator {
         jf.getContentPane().add(jp);
         jp.setLayout(new BoxLayout(jp, BoxLayout.PAGE_AXIS)); //ladnie uporzadkowane
         JTextField output = new JTextField(4);
-        output.setMargin(new Insets(0,0,0, 10));
+        output.setMargin(new Insets(0, 0, 0, 10));
         output.setText("0");
         output.setHorizontalAlignment(JTextField.RIGHT);
         output.setMaximumSize(new Dimension(390, 40));
@@ -26,81 +37,81 @@ public class newCalculator {
         jf.getContentPane().add(jp);
         jf.pack();
         jp.setLayout(new GridLayout(5, 0, 5, 15));
-        jp.setBorder(new EmptyBorder(10, 10, 10,10));
+        jp.setBorder(new EmptyBorder(10, 10, 10, 10));
 
         JPanel r1 = new JPanel();
         JPanel r2 = new JPanel();
         JPanel r3 = new JPanel();
         JPanel r4 = new JPanel();
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, beq,bpl,bmi,bmu,bdi,bclear;
+        JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, beq, bpl, bmi, bmu, bdi, bclear;
 
-        beq=new JButton("=");
+        beq = new JButton("=");
         //beq.setBackground(Color.gray);
-        beq.addActionListener(new MyActionListener());
+        beq.addActionListener(calculator);
         //r4.add(beq);
-        bpl=new JButton("+");
+        bpl = new JButton("+");
         //bpl.setBackground(Color.gray);
-        bpl.addActionListener(new MyActionListener());
+        bpl.addActionListener(calculator);
         //r1.add(bpl);
-        bmi=new JButton("-");
+        bmi = new JButton("-");
         //bmi.setBackground(Color.gray);
-        bmi.addActionListener(new MyActionListener());
+        bmi.addActionListener(calculator);
         //r2.add(bmi);
-        bmu=new JButton("*");
+        bmu = new JButton("*");
         //bmu.setBackground(Color.gray);
-        bmu.addActionListener(new MyActionListener());
+        bmu.addActionListener(calculator);
         //r3.add(bmu);
-        bdi=new JButton("/");
+        bdi = new JButton("/");
         //bdi.setBackground(Color.gray);
-        bdi.addActionListener(new MyActionListener());
+        bdi.addActionListener(calculator);
         //r4.add(bdi);
-        bclear=new JButton("C");
+        bclear = new JButton("C");
         //bclear.setBackground(Color.gray);
-        bclear.addActionListener(new MyActionListener());
+        bclear.addActionListener(calculator);
         //r4.add(bclear);
-        b0=new JButton("0");
+        b0 = new JButton("0");
         //b0.setBackground(Color.gray);
-        b0.addActionListener(new MyActionListener());
+        b0.addActionListener(calculator);
         //r4.add(b0);
-        b1=new JButton("1");
+        b1 = new JButton("1");
         //b1.setBackground(Color.gray);
-        b1.addActionListener(new MyActionListener());
+        b1.addActionListener(calculator);
         //r1.add(b1);
-        b2=new JButton("2");
+        b2 = new JButton("2");
         //b2.setBackground(Color.gray);
-        b2.addActionListener(new MyActionListener());
+        b2.addActionListener(calculator);
         //r1.add(b2);
-        b3=new JButton("3");
+        b3 = new JButton("3");
         //b3.setBackground(Color.gray);
-        b3.addActionListener(new MyActionListener());
+        b3.addActionListener(calculator);
         //r1.add(b3);
-        b4=new JButton("4");
+        b4 = new JButton("4");
         //b4.setBackground(Color.gray);
-        b4.addActionListener(new MyActionListener());
+        b4.addActionListener(calculator);
         //r2.add(b4);
-        b5=new JButton("5");
+        b5 = new JButton("5");
         //b5.setBackground(Color.gray);
-        b5.addActionListener(new MyActionListener());
+        b5.addActionListener(calculator);
         //r2.add(b5);
-        b6=new JButton("6");
+        b6 = new JButton("6");
         //b6.setBackground(Color.gray);
-        b6.addActionListener(new MyActionListener());
+        b6.addActionListener(calculator);
         //r2.add(b6);
-        b7=new JButton("7");
+        b7 = new JButton("7");
         //b7.setBackground(Color.gray);
-        b7.addActionListener(new MyActionListener());
+        b7.addActionListener(calculator);
         //r3.add(b7);
-        b8=new JButton("8");
+        b8 = new JButton("8");
         //b8.setBackground(Color.gray);
-        b8.addActionListener(new MyActionListener());
+        b8.addActionListener(calculator);
         r3.add(b8);
-        b9=new JButton("9");
-        b9.addActionListener(new MyActionListener());
+        b9 = new JButton("9");
+        b9.addActionListener(calculator);
         // b9.setBackground(Color.gray);
         // r3.add(b9);
-        for (JButton b : Arrays.asList(b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, beq,bpl,bmi,bmu,bdi,bclear)){
-            Dimension dimension=new Dimension(75,31);
+        for (JButton b : Arrays.asList(b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, beq, bpl, bmi, bmu, bdi, bclear)) {
+            Dimension dimension = new Dimension(75, 31);
             b.setPreferredSize(dimension);
             r4.add(beq);
             r1.add(bpl);
@@ -129,19 +140,32 @@ public class newCalculator {
         jp.add(r4);
 
     }
-    public void Actions(){
-
-    }
 
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() { createAndShowGUI(); }
+            public void run() {
+                createAndShowGUI();
+            }
         });
-    }}
-class MyActionListener implements java.awt.event.ActionListener {
+    }
+
+    @Override
 
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Button = "+e.getActionCommand());
+        System.out.println("Button = " + e.getActionCommand());
+        String click = e.getActionCommand();
+        switch (click) {
+            case "1", "2", "3", "4", "5", "6", "7", "8", "9", "0":
+
+        }
+
     }
 
 }
+
+/*class MyActionListener implements java.awt.event.ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Button = "+e.getActionCommand());
+    }}*/
+
